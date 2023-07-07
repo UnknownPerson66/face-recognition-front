49b06410162221dd60a71cfd640ed6b5a45707b2
UnknownPerson66 <46661756+UnknownPerson66@users.noreply.github.com>
2023-07-07 17:06:30 -0300

    -

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -4,7 +4,7 @@ import './ImageLinkForm.css'
 const ImageLinkForm = ({onInputChange, onPictureSubmit}) => {
     return (   
         <div className="ImageLinkForm center">
-            <p>{'this magic brain will detect faces in your picture. Git it a try'}</p>
+            <p>{'this app will detect faces in your picture. Git it a try'}</p>
         
             <div className='detect-box' style={{width: '700px', maxWidth: '100%', margin: 50}}> 
                 <input placeholder='Paste your link here...' className='detect-input' type='text' onChange={onInputChange} />
